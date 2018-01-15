5218daa0e71c40fdd81571b331509ce5436d0fbb
JerryNz <JerryNz@163.com>
2018-01-15 12:39:53 +0800

    The first commit for Test Project

---
+++ app/src/test/java/com/example/administrator/androidtestforjenkins/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.example.administrator.androidtestforjenkins;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+public class HelloWorldTest {
+
+}
